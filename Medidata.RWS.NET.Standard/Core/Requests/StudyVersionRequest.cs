177f2ed7bf8785e83d890b26177aa885db276df1
Matthew Koch <matthewjameskoch@gmail.com>
2018-06-14 08:44:17 -0400

    added new request types

---
+++ Medidata.RWS.NET.Standard/Core/Requests/StudyVersionRequest.cs
@@ -0,0 +1,17 @@
+﻿using Flurl;
+
+namespace Medidata.RWS.NET.Standard.Core.Requests
+{
+    public class StudyVersionRequest : VersionRequestBase
+    {
+
+        public StudyVersionRequest(string projectName, string Oid) : base(projectName, Oid)
+        {
+        }
+
+        public override string UrlPath()
+        {
+            return Url.Combine("metadata", "studies", ProjectName, "versions", Oid.ToString());
+        }
+    }
+}
