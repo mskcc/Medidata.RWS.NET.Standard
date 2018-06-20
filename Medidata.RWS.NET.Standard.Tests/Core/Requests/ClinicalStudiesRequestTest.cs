2ead976759bbcfe511ff9027c2b01c20a6922c1e
Matthew Koch <matthewjameskoch@gmail.com>
2018-06-20 11:04:58 -0400

    add usersrequest

--- Medidata.RWS.NET.Standard.Tests/Core/Requests/ClinicalStudiesRequestTest.cs
+++ Medidata.RWS.NET.Standard.Tests/Core/Requests/ClinicalStudiesRequestTest.cs
@@ -11,76 +11,74 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Medidata.RWS.NET.Standard.Tests.Core.Requests
 {
+    [TestClass]
     public class ClinicalStudiesRequestTest
     {
-        [TestClass]
-        public class StudyVersionsRequestTest
+        
+        private HttpTest _httpTest;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _httpTest = new HttpTest();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _httpTest.Dispose();
+        }
+
+
+        [TestMethod]
+        public void ClinicalStudiesRequest_can_properly_construct_a_url()
+        {
+            var request = new ClinicalStudiesRequest();
+
+            Assert.AreEqual("studies", request.UrlPath());
+
+        }
+
+
+        [TestMethod]
+        public async Task ClinicalStudiesRequest_can_format_a_response()
         {
 
-            private HttpTest _httpTest;
+            _httpTest.RespondWith(
+                new StringContent(@"<ODM FileType=""Snapshot"" FileOID="" CreationDateTime="" ODMVersion=""1.3""
+                    xmlns:mdsol=""http://www.mdsol.com/ns/odm/metadata"" xmlns=""http://www.cdisc.org/ns/odm/v1.3"">
+                     <Study OID=""Lab Test"">
+                       <GlobalVariables>
+                          <StudyName>Lab Test</StudyName>
+                          <StudyDescription />
+                          <ProtocolName>Lab Test</ProtocolName>
+                       </GlobalVariables>
+                     </Study>
+                     <Study OID=""Mediflex"">
+                       <GlobalVariables>
+                          <StudyName>Mediflex</StudyName>
+                          <StudyDescription />
+                          <ProtocolName>Mediflex</ProtocolName>
+                       </GlobalVariables>
+                     </Study>
+                    </ODM>", Encoding.UTF8, "text/xml"));
 
-            [TestInitialize]
-            public void SetUp()
+            var rwsConnection = new RwsConnection("innovate", "test", "password");
+            var request = new ClinicalStudiesRequest();
+            var result = await rwsConnection.SendRequestAsync(request) as RwsStudies;
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(RwsStudies));
+
+            var studies = new[] { "Lab Test", "Mediflex" };
+
+            foreach (var study in result)
             {
-                _httpTest = new HttpTest();
+                Assert.IsTrue(studies.Contains(study.Oid));
             }
-
-            [TestCleanup]
-            public void TearDown()
-            {
-                _httpTest.Dispose();
-            }
+               
 
 
-            [TestMethod]
-            public void ClinicalStudiesRequest_can_properly_construct_a_url()
-            {
-                var request = new ClinicalStudiesRequest();
-
-                Assert.AreEqual("studies", request.UrlPath());
-
-            }
-
-
-            [TestMethod]
-            public async Task ClinicalStudiesRequest_can_format_a_response()
-            {
-
-                _httpTest.RespondWith(
-                    new StringContent(@"<ODM FileType=""Snapshot"" FileOID="" CreationDateTime="" ODMVersion=""1.3""
-                        xmlns:mdsol=""http://www.mdsol.com/ns/odm/metadata"" xmlns=""http://www.cdisc.org/ns/odm/v1.3"">
-                         <Study OID=""Lab Test"">
-                           <GlobalVariables>
-                              <StudyName>Lab Test</StudyName>
-                              <StudyDescription />
-                              <ProtocolName>Lab Test</ProtocolName>
-                           </GlobalVariables>
-                         </Study>
-                         <Study OID=""Mediflex"">
-                           <GlobalVariables>
-                              <StudyName>Mediflex</StudyName>
-                              <StudyDescription />
-                              <ProtocolName>Mediflex</ProtocolName>
-                           </GlobalVariables>
-                         </Study>
-                        </ODM>", Encoding.UTF8, "text/xml"));
-
-                var rwsConnection = new RwsConnection("innovate", "test", "password");
-                var request = new ClinicalStudiesRequest();
-                var result = await rwsConnection.SendRequestAsync(request) as RwsStudies;
-                Assert.IsNotNull(result);
-                Assert.IsInstanceOfType(result, typeof(RwsStudies));
-
-                var studies = new[] { "Lab Test", "Mediflex" };
-
-                foreach (var study in result)
-                {
-                    Assert.IsTrue(studies.Contains(study.Oid));
-                }
-                   
-
-
-            }
         }
+        
     }
 }
---
+++ Medidata.RWS.NET.Standard.Tests/Core/Requests/ODM Adapter/UsersRequestTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Flurl;
+using Medidata.RWS.NET.Standard.Core.Requests.ODMAdapter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Medidata.RWS.NET.Standard.Tests.Core.Requests.ODMAdapter
+{
+    [TestClass]
+    public class UsersRequestTests
+    {
+        [TestMethod]
+        public void UsersRequest_can_properly_create_a_request_with_query_string_parameters()
+        {
+
+            var uRequest = new UsersRequest(ProjectName: "Mediflex", Environment: "Dev", locationoid: "101");
+
+            Assert.IsTrue(uRequest.UrlPath().Contains("datasets/Users.odm"));
+
+            Assert.IsTrue(uRequest.UrlPath().Contains(string.Format("studyoid={0}", Url.Encode(uRequest.studyoid))));
+
+            Assert.IsTrue(uRequest.UrlPath().Contains(string.Format("locationoid={0}", Url.Encode(uRequest.locationoid))));
+        }
+    }
+}
